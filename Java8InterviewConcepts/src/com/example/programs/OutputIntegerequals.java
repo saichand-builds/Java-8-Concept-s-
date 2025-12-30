cfb225d477cdd0adcd5efe7a822bc99828885f82
Inter <saichand1997.s5@gmail.com>
2025-12-31 05:27:15 +0530

    OutputIntegerequals

---
+++ Java8InterviewConcepts/src/com/example/programs/OutputIntegerequals.java
@@ -0,0 +1,15 @@
+package com.example.programs;
+
+public class OutputIntegerequals {
+	public static void main(String[] args) {
+
+		Integer a = 128, b = 128;
+
+		System.out.println("compare the values  - > " + (a == b));
+		
+		System.out.println("compare the values  - > " + (a.equals(b)));
+		
+
+	}
+
+}
