f724ee708a4b2fe0531eb6e8ace843622b8e7358
Inter <saichand1997.s5@gmail.com>
2026-01-10 20:43:50 +0530

    Map Merge Demo

---
+++ Java8InterviewConcepts/src/com/example/programs/MapMergeDemo.java
@@ -0,0 +1,21 @@
+package com.example.programs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMergeDemo {
+	
+	public static void main(String[] args) {
+		
+		Map<String, Integer> scores= new HashMap<String, Integer>();
+		scores.put("Sai", 29);
+		scores.put("HArshi",27);
+		
+		scores.merge("Sai", 5, (e1, e2)-> e1+e2);
+		scores.merge("Shafi", 3, (e1,e2)->e1+e2);
+		
+		System.out.println(" Merge the Data"  +scores);
+		
+	}
+
+}
