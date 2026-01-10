9dd2b1565577fe9167957f5dc52fdef218775444
Inter <saichand1997.s5@gmail.com>
2026-01-10 20:49:11 +0530

    Map Merge Method

--- Java8InterviewConcepts/src/com/example/programs/MapMergeDemo.java
+++ Java8InterviewConcepts/src/com/example/programs/MapMergeDemo.java
@@ -11,8 +11,11 @@ public class MapMergeDemo {
 		scores.put("Sai", 29);
 		scores.put("HArshi",27);
 		
-		scores.merge("Sai", 5, (e1, e2)-> e1+e2);
-		scores.merge("Shafi", 3, (e1,e2)->e1+e2);
+	//	scores.merge("Sai", 5, (e1, e2)-> e1+e2);
+	//	scores.merge("Shafi", 3, (e1,e2)->e1+e2);
+		
+		scores.merge("Sai", 10, Integer :: sum);
+		scores.merge("Harsh", 42,  Integer::sum);
 		
 		System.out.println(" Merge the Data"  +scores);
 		
