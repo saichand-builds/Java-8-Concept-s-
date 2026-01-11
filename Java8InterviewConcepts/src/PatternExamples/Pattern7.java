0940397f552577d731322e7bcc3bafbe4e9a3250
Inter <saichand1997.s5@gmail.com>
2026-01-11 10:51:12 +0530

    Pattern Problems

---
+++ Java8InterviewConcepts/src/PatternExamples/Pattern7.java
@@ -0,0 +1,22 @@
+package PatternExamples;
+
+public class Pattern7 {
+
+  public static void main(String[] args) {
+
+    int n =5;
+
+    for (int i =0; i <n; i++) {
+
+      for (int j =0; j <n -1 -i; j++) {
+        System.out.print("  ");
+      }
+      for (int k =0; k <=i; k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+
+    }
+  }
+
+}
